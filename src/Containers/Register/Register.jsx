1fc4162e1f1eb52648b67530ac08a09f78a149bc
Luciano Germani <lucianogermani93@gmail.com>
2023-01-25 13:12:29 +0100

    Feat: Implement message outputAttempt on register and regular expression

--- src/Containers/Register/Register.jsx
+++ src/Containers/Register/Register.jsx
@@ -14,21 +14,54 @@ const Register = () => {
         password: '',
     })
 
+    //Verification for the password
+    const [passwordVerification, setPasswordVerification] = useState()
+    //Register attempt message Hook
+    const [outputAttempt, setOutputAttempt] = useState();
+
+    //Iterate userData and assign to each key the current value
     const updateUserData = (data) => {
         setUserData({...userData, [data.target.name]: data.target.value})
     }
 
+    //Attempt to register the user
     const registerUser = async() => {
+
+        //This will search in userData.value and if empty string, return error
+        Object.values(userData).forEach((value) => {
+            if(value.length === 0){
+                setOutputAttempt("Tienes que rellenar todos los campos")
+            }
+        })
+
+        //Regular Expression
+        //Email
+        if (!userData.email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)) {
+            setOutputAttempt("Inserta un email valido");
+            return;
+        }
+        //Password with special character
+        if (!userData.password.match(/^(?=.*[*@!#%&()^~{}]).*$/)) {
+            setOutputAttempt("La password tiene que tener un caracter especial");
+            return;
+        }
+        //Check double password verification
+        if(userData.password !== passwordVerification){
+            setOutputAttempt("Las contraseñas no coinciden");
+            return;
+        }
+
+        
         try {
             const attempt = await axios.post("https://bbobras.onrender.com/api/auth/projectManagerSignIn",userData)
             if(attempt.status === 200){
                 console.log("Registrado!")
-                setTimeout(() => {
-                    window.location.reload()
-                }, 2000);
+                // setTimeout(() => {
+                //     window.location.reload()
+                // }, 2000);
             }
         } catch (error) {
-            console.log("Error ", error)
+            setOutputAttempt(error.response.data.message)
         }
     }
 
@@ -68,8 +101,12 @@ const Register = () => {
                         <p>Password:</p>
                         <input type="text" name='password' title='password' onChange={updateUserData} />
                     </div>
-                    
+                    <div className="inputBlock">
+                        <p>Repite la Password:</p>
+                        <input type="text" name='password' title='password' onChange={e => setPasswordVerification(e.target.value)} />
+                    </div>                    
                 </div>
+                    <div className="messageError">{outputAttempt}</div>
                     <button onClick={() => registerUser()}>Register</button>
             </div>
         </div>
