b429e31fff39cb7dda17c86e433ae0b4b97eee5f
Luciano Germani <lucianogermani93@gmail.com>
2023-01-26 21:24:43 +0100

    Feat: Create logic to switch from 1 component view to another in dashboard

--- src/Containers/Dashboard/Dashboard.jsx
+++ src/Containers/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logout, userData } from "../../Features/userSlice";
@@ -8,6 +8,11 @@ import UserProject from "../Project/UserProject/UserProject";
 
 
 const Dashboard = () => {
+
+  //Hooks
+  const [select,setSelect] = useState("newProject");
+
+  //Const
   const userInfo = useSelector(userData);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -26,13 +31,19 @@ const Dashboard = () => {
       <div className="sideBarContainer">
         <div className="sideBarMenu">
           <div className="sideBarSelectionContainer">
-            <h2>Nuevo Proyecto</h2>
-            <h2>Mis Proyectos</h2>
+            <h2 onClick={() => setSelect("newProject")}>Nuevo Proyecto</h2>
+            <h2 onClick={() => setSelect("userProject")}>Mis Proyectos</h2>
             <h2>Perfil</h2>
           </div>
         </div>
         <div className="renderContainer">
-            {<NewProject></NewProject>}
+            {
+                select === "newProject"? (
+                    <NewProject></NewProject>
+                ): select === "userProject" ? (
+                    <UserProject></UserProject>
+                ): null
+            }
         </div>
       </div>
       <div className="chatComponentContaine"></div>
