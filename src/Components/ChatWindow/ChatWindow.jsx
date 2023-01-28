ba8c5dce5f5b5c963199142b1d41089df58dbefb
Germanilu <lucianogermani93@gmail.com>
2023-01-28 16:47:43 +0100

    Feat: Send selectedChat() to Chat.jsx(HIJO) to set ID of chat, then sending the state to ChatWindow

---
+++ src/Components/ChatWindow/ChatWindow.jsx
@@ -0,0 +1,13 @@
+import "./ChatWindow.scss"
+
+const ChatWindow = ({openWindow}) => {
+
+    console.log(openWindow, "estoy en chatwindow")
+    return (
+        <div className="chatWindowDesign">
+            Soy la chat
+        </div>
+    );
+}
+
+export default ChatWindow;
--- src/Containers/Chat/Chat.jsx
+++ src/Containers/Chat/Chat.jsx
@@ -4,20 +4,20 @@ import {userData} from "../../Features/userSlice"
 import axios from "axios"
 import "./Chat.scss"
 
-const Chat = () => {
+const Chat = ({selectedChat}) => {
 
     const [businessChat, setBusinessChats] = useState({})
     const [workerChats, setWorkerChats] = useState({});
-    
+
     //Const
     const userInfo = useSelector(userData);
 
-    console.log(userInfo.user_role)
+    
     useEffect(() => {
         getChat()
     },[])
 
-    
+    //Get all the available chat for the user
     const getChat = async() => {
         try {
             let config = {
@@ -34,6 +34,8 @@ const Chat = () => {
         }
     }
 
+
+
     //If user is PM
     if(userInfo.user_role === "63c6963759433440683992f3"){
         return(
@@ -44,7 +46,7 @@ const Chat = () => {
                     businessChat.length > 0 && (
                        businessChat.map((e) => {
                             return(
-                                <div className="chatRow" key={e._id}>{e.clientName}</div>
+                                <div className="chatRow" key={e._id} onClick={() => selectedChat(e._id) }>{e.clientName}</div>
                             )
                         })
                     )
